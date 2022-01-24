77ce177d9f91ecf1d2facec8d1139aa9a474a1a9
shsal <s.saliba@s-v.de>
2022-01-24 11:47:06 +0100

    Project Updated

---
+++ _nuxt/static/1643021152/Categories/electronics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643021152",layout:"default",error:a,state:{users:[]},serverRendered:true,routePath:"\u002FCategories\u002Felectronics",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
